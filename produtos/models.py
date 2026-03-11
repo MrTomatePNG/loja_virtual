f51b69371c072a3554913cb8db26eaf8e141fd8c
MrTomatePNG <mrtomate360@outlook.com>
2026-03-11 11:57:37 -0400

    UPDATE: indexes, slug field e image field
    indices atualizados
    suprte a upload de imagens
    estoque mudou de integer pra positive integer (nao faz sentido uma
    estoque negatvio... eu acho)
    `atualizado_em` pra marcar a ultima mudança
    slug pra criar urls como produto/:slug/detalhes

--- produtos/models.py
+++ produtos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Index, PositiveIntegerField
 
 
 # Create your models here.
@@ -14,6 +15,9 @@ class Categoria(models.Model):
         ordering = ["nome"]
         verbose_name = "categoria"
         verbose_name_plural = "categorias"
+        indexes = [
+            Index(fields=["nome"]),
+        ]
 
     objects: type[models.Manager["Categoria"]]
 
@@ -23,8 +27,12 @@ class Produto(models.Model):
     nome = models.CharField(max_length=200)
     descricao = models.TextField(blank=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-    estoque = models.IntegerField(default=0)
+    estoque = models.PositiveIntegerField(default=0)
+    disponivel = models.BooleanField(default=True)
+    slug = models.SlugField(unique=True, max_length=200)
+    imagem = models.ImageField(upload_to="produtos/%Y/%m/%d", blank=True)
     criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
     categoria = models.ForeignKey(
         Categoria, on_delete=models.CASCADE, related_name="produtos"
     )
@@ -33,6 +41,7 @@ class Produto(models.Model):
         ordering = ["nome"]
         verbose_name = "produto"
         verbose_name_plural = "produtos"
+        indexes = [Index(fields=["nome"]), Index(fields=["categoria", "disponivel"])]
 
     objects: type[models.Manager["Produto"]]
 
