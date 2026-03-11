858522d29fd693d1e40f31bce3183bc49dd087bc
MrTomatePNG <mrtomate360@outlook.com>
2026-03-10 23:14:22 -0400

    URLS add: deifnido o root do projeto onde sera a "vitrine"

--- produtos/views.py
+++ produtos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from produtos.models import Produto
+
+
 # Create your views here.
+def listar_produtos(request):
+    produtos = Produto.objects.all()
+    return render(request, "produtos/lista.html", {"produtos": produtos})
