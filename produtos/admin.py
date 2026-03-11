adf9ea52dd962efbd4d8773f09a627a97dbacd8a
MrTomatePNG <mrtomate360@outlook.com>
2026-03-11 13:21:17 -0400

    UPDATE: modelos especificos para o admin
    pre-população de campos: nome do produto é usado como slug tanto em
    produto quando em categoria.
    agora um produto é possivel ver uma lista com as categorias disponeveis
    no apinel de admin ou criar uma durante a criação de uma produto

--- produtos/admin.py
+++ produtos/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 
 from .models import Categoria, Produto
 
-admin.site.register(Produto)
-admin.site.register(Categoria)
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ["nome", "slug"]
+    prepopulated_fields = {"slug": ("nome",)}
+
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = [
+        "nome",
+        "slug",
+        "preco",
+        "estoque",
+        "disponivel",
+        "criado_em",
+        "atualizado_em",
+    ]
+    list_filter = ["disponivel", "criado_em", "atualizado_em", "categoria"]
+    list_editable = ["preco", "estoque", "disponivel"]
+    prepopulated_fields = {"slug": ("nome",)}
